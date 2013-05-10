a63b185d9700dd17af0515a4bf5a3b0d7dc3b158
Emmanuel <aleksgs10@gmail.com>
2013-05-10 16:29:52 -0500

    Merge collided boxes

--- motion.py
+++ motion.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import Image
 import cv
+import sys
 
 c = cv.CreateCameraCapture(0)
 cv.SetCaptureProperty( c, cv.CV_CAP_PROP_FRAME_WIDTH, 320 )
@@ -44,10 +45,7 @@ def merge_collided_bboxes( bbox_list ):
         return merge_collided_bboxes( bbox_list )
   return bbox_list
 
-
-while(1):
-  camera_image = cv.QueryFrame( c )
-  display_image = cv.CloneImage( camera_image )
+def get_motion_mask(camera_image):
   color_image = cv.CloneImage( camera_image )
   cv.Smooth( color_image, color_image, cv.CV_GAUSSIAN, 19, 0 )
   cv.RunningAvg( color_image, running_average_image, 0.320, None )
@@ -58,11 +56,28 @@ while(1):
   # Smooth and threshold again to eliminate "sparkles"
   cv.Smooth( grey_image, grey_image, cv.CV_GAUSSIAN, 19, 0 )
   cv.Threshold( grey_image, grey_image, 240, 255, cv.CV_THRESH_BINARY )
+  return grey_image
+
+
+while(1):
+  camera_image = cv.QueryFrame( c )
+  #display_image = cv.CloneImage( camera_image )
+  prev = get_motion_mask(camera_image)
+
+  camera_image = cv.QueryFrame( c )
+  next = get_motion_mask(camera_image)  
+ 
   #cv.ShowImage('e2', grey_image)
   bounding_box_list = []
-  contour = cv.FindContours( grey_image, mem_storage, cv.CV_RETR_CCOMP, cv.CV_CHAIN_APPROX_SIMPLE )
+  contour = cv.FindContours( prev, mem_storage, cv.CV_RETR_CCOMP, cv.CV_CHAIN_APPROX_SIMPLE )
   #print len(contour)
   if len(list(contour)) > 0:
+    prev_pyramid = cv.CreateImage (cv.GetSize (frame), 8, 1)
+    pyramid = cv.CreateImage (cv.GetSize (frame), 8, 1)
+    features, status, track_error = cv.CalcOpticalFlowPyrLK(prev, next, pyramid, prev_pyramid, contour,
+                                  (10, 10), 5,  (cv.CV_TERMCRIT_ITER|cv.CV_TERMCRIT_EPS, 20, 0.03), 0)
+    print features
+#    sys.exit()
     bounding_rect = cv.BoundingRect( list(contour) )
     while contour:
       point1 = ( bounding_rect[0], bounding_rect[1] )
@@ -71,13 +86,16 @@ while(1):
       bounding_box_list.append( ( point1, point2 ) )
       polygon_points = cv.ApproxPoly( list(contour), mem_storage, cv.CV_POLY_APPROX_DP )
 
-      cv.FillPoly( grey_image, [ list(polygon_points), ], cv.CV_RGB(255,255,255), 0, 0 )
-      cv.PolyLine( display_image, [ polygon_points, ], 0, cv.CV_RGB(255,255,255), 1, 0, 0 )
+      cv.FillPoly( prev, [ list(polygon_points), ], cv.CV_RGB(255,255,255), 0, 0 )
+      cv.PolyLine( camera_image, [ polygon_points, ], 0, cv.CV_RGB(255,255,255), 1, 0, 0 )
 
       contour = contour.h_next()
-  box_areas = []
-  for box in bounding_box_list:
-    cv.Rectangle( display_image, box[0], box[1], (0, 255, 0), 1 )
+
+
+#  box_areas = []
+#  for box in bounding_box_list:
+#    cv.Rectangle( camera_image, box[0], box[1], (0, 255, 0), 1 )
+#  cv.CalcOpticalFlowLK(prev, curr, winSize, velx, vely)
 #  for box in bounding_box_list:
 #    box_width = box[right][0] - box[left][0]
 #    box_height = box[bottom][0] - box[top][0]
@@ -95,8 +113,11 @@ while(1):
 #  bounding_box_list = merge_collided_bboxes( trimmed_box_list )
 #  for box in bounding_box_list:
 #    cv.Rectangle( display_image, box[0], box[1], cv.CV_RGB(0,255,0), 1 )
+#  crit = ( cv.CV_TERMCRIT_EPS | cv.CV_TERMCRIT_ITER, 10, 1)
 
-  cv.ShowImage('e2', display_image)
+#  (iters, (area, value, rect), track_box) = cv.CamShift(grey_image, (box[0][0], box[0][1], box[1][0], box[1][1]), crit)
+#  cv.EllipseBox( camera_image, track_box, cv.CV_RGB(255,0,0), 3, cv.CV_AA, 0 )
+  cv.ShowImage('e2', camera_image)
 
   if cv2.waitKey(5) == 27:
     break

72b66ef0eee32268723537427c62bba144846dd7
Emmanuel <aleksgs10@gmail.com>
2013-05-11 21:15:58 -0500

    Lectura de videos agregada

--- motion.py
+++ motion.py
@@ -1,38 +1,6 @@
 import cv
 import math
-########################################################################################
-#Variables importantes utilizadas
-########################################################################################
-WIDTH = 320 #Ancho de video a usar
-HEIGHT = 240 #Alto de video a usar
-AREA = WIDTH*HEIGHT #Numero de pixeles/Area total de los frames
-MIN_PERCENT = 0.1 #Porcentaje minimo de pixeles que debe tener un objeto en movimiento
-GAUSSIAN_BLUR_FACTOR_1 = 3 #Tamano de la matriz usada para eliminar ruido la primera vez
-GAUSSIAN_BLUR_FACTOR_2 = 19 #Tamano de la matriz usada para eliminar ruido la segunda vez
-BINARY_THRESHOLD_1 = 2 #Umbral de binarizacion inicial
-BINARY_THRESHOLD_2 = 240 #Umbral de binarizacion final
-DILATION_FACTOR = 1 #Tamano de la matriz usada para dilatar las manchas de movimiento
-SHOW_MOVEMENT_CONTOUR = True #Ver o no contornos de las manchas de movimiento
-SHOW_MOVEMENT_AREA = False #Ver o no las manchas de movimiento
-
-########################################################################################
-#Propiedades de la captura de imagenes desde la camara.
-########################################################################################
-c = cv.CreateCameraCapture(0) #Usar camara 0 (camara web)
-cv.SetCaptureProperty( c, cv.CV_CAP_PROP_FRAME_WIDTH, WIDTH ) #Ajustar ancho de video
-cv.SetCaptureProperty( c, cv.CV_CAP_PROP_FRAME_HEIGHT, HEIGHT ) #Ajustar altura de video
-
-########################################################################################
-#Inicializacion de variables. En esta version de OpenCV para Python en muchas funciones
-#es necesario pasar como argumento la variable en la cual queremos que retorne un cierto
-#dato esa funcion, y debe ser del mismo tipo de dato que la funcion utiliza.
-########################################################################################
-frame = cv.QueryFrame( c ) #Se toma un frame desde la camara para crear copias
-grey_image = cv.CreateImage( cv.GetSize(frame), cv.IPL_DEPTH_8U, 1 ) #copia usada para binarizacion y grescale
-running_average_image = cv.CreateImage( cv.GetSize(frame), cv.IPL_DEPTH_32F, 3 ) #copia usada para promediado de frames
-running_average_in_display_color_depth = cv.CloneImage( frame ) #copia usada para conversion de escala
-mem_storage = cv.CreateMemStorage(0) #buffer de memoria usado por algunas funciones
-difference = cv.CloneImage( frame ) #copia usada para almacenar la diferencia entre frames
+import sys
 
 ########################################################################################
 #Funcion que a partir de una lista de bounding box busca aquellos que se sobreponen
@@ -68,15 +36,53 @@ def get_collided_bboxes(bbox_list):
 def get_motion_mask(camera_image):
   color_image = cv.CloneImage( camera_image ) #copia usada para no modificar la original
   cv.Smooth( color_image, color_image, cv.CV_GAUSSIAN, GAUSSIAN_BLUR_FACTOR_1, 0 ) #smooth/blur inicial para eliminar ruido
-  cv.RunningAvg( color_image, running_average_image, 0.320, None )#obtencion de un promedio de frames
-  cv.ConvertScale( running_average_image, running_average_in_display_color_depth, 1.0, 0.0 ) #conversion de escala
-  cv.AbsDiff( color_image, running_average_in_display_color_depth, difference ) #diferencia entre la imagen promedio y la original
-  cv.CvtColor( difference, grey_image, cv.CV_RGB2GRAY )#conversion de la imagen a escala de grises
-  cv.Threshold( grey_image, grey_image, BINARY_THRESHOLD_1, 255, cv.CV_THRESH_BINARY )#binarizacion de la imagen
-  cv.Dilate(grey_image, grey_image, None, DILATION_FACTOR) #dilatacion de los pixeles blancos para juntar manchas de movimiento cercanas
-  cv.Smooth( grey_image, grey_image, cv.CV_GAUSSIAN, GAUSSIAN_BLUR_FACTOR_2, 0 ) #otro smooth aplicado para eliminar pequenas manchas
-  cv.Threshold( grey_image, grey_image, BINARY_THRESHOLD_2, 255, cv.CV_THRESH_BINARY ) #rebinarizacion para eliminar pequenas manchas
-  return grey_image
+  return color_image
+
+def distance(p1, p2):
+  x1, y1 = p1
+  x2, y2 = p2
+  return math.sqrt( (y2 - y1)**2 + (x2 - x1)**2)
+
+
+
+########################################################################################
+#Variables importantes utilizadas
+########################################################################################
+WIDTH = 320 #Ancho de video a usar
+HEIGHT = 240 #Alto de video a usar
+DIAGONAL = distance((0, 0), (WIDTH, HEIGHT))
+AREA = WIDTH*HEIGHT #Numero de pixeles/Area total de los frames
+MIN_PERCENT = 0.1 #Porcentaje minimo de pixeles que debe tener un objeto en movimiento
+GAUSSIAN_BLUR_FACTOR_1 = 3 #Tamano de la matriz usada para eliminar ruido la primera vez
+GAUSSIAN_BLUR_FACTOR_2 = 3 #Tamano de la matriz usada para eliminar ruido la segunda vez
+BINARY_THRESHOLD_1 = 5 #Umbral de binarizacion inicial
+BINARY_THRESHOLD_2 = 240 #Umbral de binarizacion final
+DILATION_FACTOR = 0 #Tamano de la matriz usada para dilatar las manchas de movimiento
+SHOW_MOVEMENT_CONTOUR = True #Ver o no contornos de las manchas de movimiento
+SHOW_MOVEMENT_AREA = False #Ver o no las manchas de movimiento
+
+########################################################################################
+#Propiedades de la captura de imagenes desde la camara.
+########################################################################################
+c = cv.CreateCameraCapture(0) #Usar camara 0 (camara web)
+if ".avi" in sys.argv[1]:
+  c = cv.CaptureFromFile( sys.argv[1] ) #Usar camara 0 (camara web)
+cv.SetCaptureProperty( c, cv.CV_CAP_PROP_FRAME_WIDTH, WIDTH ) #Ajustar ancho de video
+cv.SetCaptureProperty( c, cv.CV_CAP_PROP_FRAME_HEIGHT, HEIGHT ) #Ajustar altura de video
+
+########################################################################################
+#Inicializacion de variables. En esta version de OpenCV para Python en muchas funciones
+#es necesario pasar como argumento la variable en la cual queremos que retorne un cierto
+#dato esa funcion, y debe ser del mismo tipo de dato que la funcion utiliza.
+########################################################################################
+frame = cv.QueryFrame( c ) #Se toma un frame desde la camara para crear copias
+grey_image = cv.CreateImage( cv.GetSize(frame), cv.IPL_DEPTH_8U, 1 ) #copia usada para binarizacion y grescale
+motion1 = cv.CreateImage( cv.GetSize(frame), cv.IPL_DEPTH_8U, 1 ) #copia usada para binarizacion y grescale
+motion2 = cv.CreateImage( cv.GetSize(frame), cv.IPL_DEPTH_8U, 1 ) #copia usada para binarizacion y grescale
+running_average_image = cv.CreateImage( cv.GetSize(frame), cv.IPL_DEPTH_32F, 3 ) #copia usada para promediado de frames
+running_average_in_display_color_depth = cv.CloneImage( frame ) #copia usada para conversion de escala
+mem_storage = cv.CreateMemStorage(0) #buffer de memoria usado por algunas funciones
+difference = cv.CloneImage( frame ) #copia usada para almacenar la diferencia entre frames
 
 ########################################################################################
 #Loop infinito para ejecutar las rutinas de deteccion de movimiento hasta
@@ -85,8 +91,9 @@ def get_motion_mask(camera_image):
 pi = 3.14159265358979323846
 while True:
   frame = cv.QueryFrame( c ) #imagen obtenida de la camara
+  if frame == None: break
   frame1 = cv.CloneImage( frame )
-  #frame = get_motion_mask(frame) #imagen tratada con los filtros para obtener zonas en movimiento
+  frame = get_motion_mask(frame) #imagen tratada con los filtros para obtener zonas en movimiento
   eig_image = cv.CreateImage(cv.GetSize(frame), cv.IPL_DEPTH_32F, 1)
   temp_image = cv.CreateImage(cv.GetSize(frame), cv.IPL_DEPTH_32F, 1)
   frame1_1C = cv.CreateImage( cv.GetSize(frame), cv.IPL_DEPTH_8U, 1 ) 
@@ -99,15 +106,20 @@ while True:
   #cv.ConvertImage(frame, frame1, cv.CV_CVTIMG_FLIP)
 
   frame = cv.QueryFrame( c ) #imagen obtenida de la camara
+  if frame == None: break
+  frame = get_motion_mask(frame) #imagen tratada con los filtros para obtener zonas en movimiento
   cv.ConvertImage(frame, frame2_1C, cv.CV_CVTIMG_FLIP)
 
   qualityLevel = 0.1
   minDistance = 5
   number_of_features = 400
+  #frame1_features = cv.FindContours( frame1_1C, mem_storage, cv.CV_RETR_CCOMP, cv.CV_CHAIN_APPROX_SIMPLE )
   frame1_features = cv.GoodFeaturesToTrack(frame1_1C, eig_image, temp_image, number_of_features, qualityLevel, minDistance, None, 3, False)
 
   frame2_features, status, track_error = cv.CalcOpticalFlowPyrLK(frame1_1C, frame2_1C, pyramid1, pyramid2, frame1_features, 
                             (3, 3), 5,  (cv.CV_TERMCRIT_ITER|cv.CV_TERMCRIT_EPS, 20, 0.03), 0)
+
+  
   for i in range(number_of_features):
     try:
       p = frame1_features[i]
@@ -116,17 +128,19 @@ while True:
       angle = math.atan2(p[1] - q [1], p[0] - q[0])
       hypotenuse = math.sqrt(math.pow(p[1] - q[1], 2) + math.pow(p[0] - q[0], 2))
       q = (int(p[0] - 3*hypotenuse * math.cos(angle)), int(p[1] - 3*hypotenuse * math.sin(angle)))
-
+      print angle
+      if distance(p, q) > DIAGONAL * 0.5:
+        continue
       cv.Line( frame1, p, q, cv.CV_RGB(255, 0, 0), 1, cv.CV_AA, 0 )
 
       p = (int(q[0] + 9 * math.cos(angle + pi / 4)), int(q[1] + 9 * math.sin(angle + pi/4)))
       cv.Line( frame1, p, q, cv.CV_RGB(255, 0, 0), 1, cv.CV_AA, 0 )
       p = (int(q[0] + 9 * math.cos(angle - pi / 4)), int(q[1] + 9 * math.sin(angle - pi/4)))
       cv.Line( frame1, p, q, cv.CV_RGB(255, 0, 0), 1, cv.CV_AA, 0 )
-    except IndexError:
+    except:
       break
   
   cv.ShowImage('e2',frame1) #mostrar los resultados en el feed de video
-
   if cv.WaitKey(5) == 27: #si se presiona la tecla esc
     break #salir del while infinito para terminar
+print "Fin"
